28b0cca1e099b9074fc65ecc2882746532c78ebf
Sneha7890 <sneofficialid@gmail.com>
2025-02-01 17:35:45 +0530

    day1

---
+++ wipro_java_project/src/com/wipro/java/MyClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+
+class MyClass {
+    int num = 5;
+ 
+    public void changeValue(int num) {
+        this.num = num;
+    }
+ 
+
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();// Creating object
+        obj.changeValue(10); // calling the method
+        System.out.println(obj.num);
+    }
+}
