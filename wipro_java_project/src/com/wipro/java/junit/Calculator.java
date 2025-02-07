2b8c4530480bdbb999370101c7711b735e1eb149
Sneha7890 <sneofficialid@gmail.com>
2025-02-07 13:36:39 +0530

    day6

---
+++ wipro_java_project/src/com/wipro/java/junit/Calculator.java
@@ -0,0 +1,16 @@
+package com.wipro.java.junit;
+/*
+ * Creating calculator class
+ * Junit
+ */
+public class Calculator {
+//Constructor
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+
+}
